8547757c35602fd4475accf4eacd59d79b9d75d2
Owen Luke <hi.owen.luke@gmail.com>
2019-12-09 16:59:47 +0800

    Update index.js

--- benchmark/dooms/client/index.js
+++ benchmark/dooms/client/index.js
@@ -2,16 +2,11 @@
 const be = require('benchmark-easy')();
 const services = require('./lib/doomsClient');
 
-let sb;
-
 const main = async () => {
+	const {sb} = await services();
 	const result = await sb.test();
 	// console.log(result);
 };
 
 // module.exports = main;
-
-(async () => {
-	sb = (await services()).sb;
-	be.start(main, 10000);
-})();
+be.start(main, 10000);
