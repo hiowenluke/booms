3b9d573cc66272c83271e1a6b3c570d7ddb0a21a
Owen Luke <hi.owen.luke@gmail.com>
2019-12-12 14:36:28 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 const exec = require('child_process').exec;
 const test = require('./test');
 
-const wait = async (ms = 1000) => {
+const wait = (ms = 1000) => {
 	return new Promise(resolve => {
 		setTimeout(() => {resolve()}, ms);
 	})
@@ -11,18 +11,18 @@ const wait = async (ms = 1000) => {
 describe('Dooms', () => {
 
 	before(async () => {
-		exec('node ../example/service1');
-		exec('node ../example/service2');
+		exec('node ../examples/service1');
+		exec('node ../examples/service2');
 
 		// Waiting for all services are ready
 		await wait();
 	});
 
 	it('object style', async () => {
-		await test('../example/client-with-object-style/index');
+		await test('../examples/client-with-object-style/index');
 	});
 
 	it('message style', async () => {
-		await test('../example/client-with-message-style/index');
+		await test('../examples/client-with-message-style/index');
 	});
 });
