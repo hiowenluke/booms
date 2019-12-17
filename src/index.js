34163b989650c18426b2413cbf0c689f2932713f
Owen Luke <hi.owen.luke@gmail.com>
2019-12-17 17:12:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
 const caller = require('caller');
 const service = require('./service');
-const client = require('./client');
 const rpc = require('./rpc');
 const omg = require('./omg');
 
@@ -11,15 +10,11 @@ const me = {
 		return await service.init(pathToCaller, ...args);
 	},
 
-	initClient(...args) {
-		return client.init(...args);
-	},
-
 	initRpc(...args) {
 		return rpc.init(...args);
 	},
 
-	async omg(...args) {
+	async initClient(...args) {
 		const pathToCaller = caller();
 		return await omg.do(pathToCaller, ...args);
 	}
