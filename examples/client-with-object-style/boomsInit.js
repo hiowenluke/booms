5d24f2e793dd766a8dea54d9ee50151d16547dc9
Owen Luke <hi.owen.luke@gmail.com>
2019-12-17 17:12:23 +0800

    Update boomsInit.js

--- examples/client-with-object-style/boomsInit.js
+++ examples/client-with-object-style/boomsInit.js
@@ -1,8 +1,9 @@
 
+// This options can be omitted if options.redis.host is 'localhost'
 const options = {
 	redis: {
 		host: 'localhost',
 	}
 };
 
-require('../../src').omg(options);
+require('../../src').initClient(options);
