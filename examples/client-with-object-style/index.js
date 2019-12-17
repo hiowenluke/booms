dfd22d0b17e575c284966ab0ac24f5949c3520ec
Owen Luke <hi.owen.luke@gmail.com>
2019-12-17 18:30:57 +0800

    Update index.js

--- examples/client-with-object-style/index.js
+++ examples/client-with-object-style/index.js
@@ -1,7 +1,7 @@
 
 // Note:
 // 		Please run ./boomsInit.js first to fetch the remote services definitions
-// 		and save the data files to ./lib/services before run this file
+// 		and save the data files to directory ./lib/services before run this file.
 
 const {s1, s2} = require('./lib/services');
 
