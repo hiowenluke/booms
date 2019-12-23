cbccaacc34ad8fc1e7da8885f4325437b50e80d2
Owen Luke <hi.owen.luke@gmail.com>
2019-12-23 18:52:51 +0800

    update examples

---
+++ examples/client-lite/index.js
@@ -0,0 +1,18 @@
+
+const services = require('./boomsServices');
+
+const main = async () => {
+	const {s1, s2} = await services();
+
+	let result;
+	result = await s1.about();
+	console.log(result); // "Server #1"
+
+	result = await s2.about();
+	console.log(result); // "Server #2"
+
+	result = await s1.say.hi('owen', 100);
+	console.log(result); // { msg: 'Hi, I am owen, 100 years old.' }
+};
+
+main();
