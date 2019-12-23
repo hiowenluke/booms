a3c638c00458212fa26826fdacaa37dc5cbf7bb3
Owen Luke <hi.owen.luke@gmail.com>
2019-12-23 17:26:15 +0800

    omg.js => omg/index.js

---
+++ src/client/omg/index.js
@@ -0,0 +1,143 @@
+
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+const fx = require('fs-extra');
+const keyPaths = require('keypaths');
+
+const replaceInFile = require('../../__lib/replaceInFile');
+const omgConfig = require('./config');
+
+let isInitialized;
+
+const copyFilesToTemp = () => {
+	const destFolderPath = path.resolve(__dirname, omgConfig.tempPath);
+	if (!fs.existsSync(destFolderPath)) {
+		fs.mkdirSync(destFolderPath);
+	}
+
+	const sourceFolderPath = path.resolve(__dirname, omgConfig.boomsServicesPath);
+	fx.copySync(sourceFolderPath, destFolderPath);
+};
+
+const getClientRoot = (parentFilename) => {
+	const seekFile = (parentFolder, filename) => {
+		let p = parentFolder;
+
+		while (p !== '/') {
+			const dest = p + '/' + filename;
+			if (fs.existsSync(dest)) {
+				return dest;
+			}
+			p = path.resolve(p, '..');
+		}
+	};
+
+	const parentFolder = path.resolve(parentFilename, '..');
+	const userConfigFile = seekFile(parentFolder, 'boomsConfig.js') || seekFile(parentFolder, '.boomsConfig.js');
+	const userRoot = path.resolve(userConfigFile, '..');
+
+	return userRoot;
+};
+
+const getUserConfig = (clientRoot) => {
+	const filenames = ['boomsConfig.js', '.boomsConfig.js'];
+	const filename = filenames.find(filename => fs.existsSync(clientRoot + '/' + filename));
+	if (!filename) {
+		throw new Error(`Can not find boomsConfig.js in current project root path`);
+	}
+
+	return require(clientRoot + '/' + filename);
+};
+
+const getRawServersInfos = (userConfig) => {
+	const cmdFilePath = path.resolve(__dirname, './createRawServersInfos.js');
+	cp.execSync(`node ${cmdFilePath} '${JSON.stringify(userConfig)}'`);
+
+	const dataFilePath = path.resolve(__dirname, omgConfig.tempPath + '/rawServersInfos.js');
+	const serversInfos = require(dataFilePath);
+
+	return serversInfos;
+};
+
+const getServicesApis = (rawInfos) => {
+	const data = {};
+	const names = Object.keys(rawInfos);
+
+	for (let i = 0; i < names.length; i ++) {
+		const name = names[i];
+		const info = rawInfos[name];
+		const {apis} = info;
+		const obj = keyPaths.toObject(apis);
+		data[name] = obj;
+	}
+
+	return data;
+};
+
+const getServersInfosFromRaw = (rawInfos) => {
+
+	// Only need host and port of info
+	const names = Object.keys(rawInfos);
+	const infos = {};
+
+	for (let i = 0; i < names.length; i ++) {
+		const name = names[i];
+		const info = rawInfos[name];
+		const {host, port} = info;
+		infos[name] = {host, port};
+	}
+
+	return infos;
+};
+
+const writeToDataFile = (clientRoot, infos, apis) => {
+
+	// For Booms client running
+	const filePath = path.resolve(__dirname, omgConfig.tempPath + '/data.js');
+	replaceInFile(filePath, '`{serversInfos}`', infos);
+	replaceInFile(filePath, '`{servicesApis}`', apis);
+
+	// For user to view all apis information.
+	const sourceFile = filePath;
+	const targetFile = clientRoot + '/boomsServices.js';
+	fs.copyFileSync(sourceFile, targetFile);
+};
+
+const me = {
+	do(parentFilename) {
+		if (!isInitialized) {
+			const clientRoot = getClientRoot(parentFilename);
+			const userConfig = getUserConfig(clientRoot);
+			const {timer} = userConfig;
+
+			copyFilesToTemp();
+			this.once(clientRoot, userConfig);
+
+			isInitialized = 1;
+
+			// The latest updated apis information will only
+			// take effect the next time the client runs.
+			if (timer) {
+				setInterval(async () => {
+					await this.once(clientRoot, userConfig);
+				}, timer * 1000);
+			}
+		}
+
+		// Now that there is complete data in the temp directory,
+		// it is required and returned to the parent module.
+		return require(omgConfig.tempPath);
+	},
+
+	once(clientRoot, userConfig) {
+		const rawServersInfos = getRawServersInfos(userConfig);
+		const servicesApis = getServicesApis(rawServersInfos);
+
+		const serversInfos = getServersInfosFromRaw(rawServersInfos);
+		writeToDataFile(clientRoot, serversInfos, servicesApis);
+	}
+};
+
+module.exports = me;
