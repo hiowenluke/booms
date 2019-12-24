db54c110ee9a8bb3349ad56818c3520ca7bb0f5c
Owen Luke <hi.owen.luke@gmail.com>
2019-12-24 12:36:38 +0800

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -4,25 +4,26 @@ const kdo = require('kdo');
 const path = require('path');
 const keyPaths = require('keypaths');
 
-const config = require('../__config');
 const myJson = require('../__lib/myJson');
 const myRedis = require('../__lib/myRedis');
 const rpcArgs = require('../__lib/rpcArgs');
+
 const ports = require('./ports');
+const parseUserConfig = require('./parseUserConfig');
 
 const me = {
 	source: {},
 
 	async init(caller, ...args) {
 		try {
-			const {boomsConfig, serverConfig, redisConfig} = config.getAllConfigurations(args);
+			const {boomsConfig, redisConfig} = parseUserConfig(args);
 			myRedis.init(redisConfig);
 			ports.init(myRedis);
 
 			await this.initSource(caller, boomsConfig);
-			await this.calcServerPort(boomsConfig, serverConfig);
-			await this.saveServerData(boomsConfig, serverConfig, redisConfig);
-			await this.createServer(boomsConfig, serverConfig);
+			await this.calcServerPort(boomsConfig);
+			await this.saveServerData(boomsConfig);
+			await this.createServer(boomsConfig);
 
 			myRedis.disconnect();
 		}
@@ -37,13 +38,12 @@ const me = {
 		this.source = kdo(root + '/' + dir);
 	},
 
-	async calcServerPort(boomsConfig, serverConfig) {
-		serverConfig.port = await ports.calc(boomsConfig.name);
+	async calcServerPort(boomsConfig) {
+		boomsConfig.port = await ports.calc(boomsConfig.name);
 	},
 
-	async saveServerData(boomsConfig, serverConfig) {
-		const {name} = boomsConfig;
-		const {host, port} = serverConfig;
+	async saveServerData(boomsConfig) {
+		const {name, host, port} = boomsConfig;
 
 		const apis = keyPaths.toPaths(this.source);
 		const data = {host, port, apis};
@@ -51,8 +51,8 @@ const me = {
 		await myRedis.saveServerData(name, data);
 	},
 
-	async createServer(boomsConfig, serverConfig) {
-		const {name} = boomsConfig;
+	async createServer(boomsConfig) {
+		const {name, host, port} = boomsConfig;
 
 		const server = net.createServer(sock => {
 			sock.on('data', async data => {
@@ -78,9 +78,7 @@ const me = {
 			});
 		});
 
-		const {host, port} = serverConfig;
 		server.listen(port, host);
-
 		console.log(`Server ${name} listening on ${host}:${port}`);
 	}
 };
