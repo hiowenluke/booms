1344166103b61e7503aa05470b1c20d22aa02601
Owen Luke <hi.owen.luke@gmail.com>
2019-12-29 17:14:53 +0800

    Update services.js

--- services.js
+++ services.js
@@ -1,12 +1,34 @@
+const servers = {
+    s1: {
+        host: "localhost",
+        port: 30201
+    },
+    s2: {
+        host: "localhost",
+        port: 30202
+    }
+};
 
-const servers = `{serversInfos}`;
+const apis = {
+    s1: {
+        about: {},
+        callback: {},
+        obj: {
+            do: {}
+        },
+        say: {
+            hi: {}
+        }
+    },
+    s2: {
+        about: {}
+    }
+};
 
-const apis = `{servicesApis}`;
-
-module.exports = apis;
+module.exports = apis;                
 
 (() => {
 	const parentFilename = module.parent.filename;
 	const booms = require('.');
-	booms.client.fetchServices(parentFilename);
+	module.exports = booms.client.fetchServices(parentFilename);
 })();
