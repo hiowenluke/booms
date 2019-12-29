b65da21e17b75c8e55d97e91baa2d8e233b636de
Owen Luke <hi.owen.luke@gmail.com>
2019-12-29 16:58:50 +0800

    Update boomsConfig.js

--- examples/client/boomsConfig.js
+++ examples/client/boomsConfig.js
@@ -13,10 +13,6 @@ const me = {
 	// If it is omitted or 'all', Booms will fetches all.
 	// Or "s1" if you just need this one.
 	servers: ['s1', 's2'],
-
-	// Whether to create boomsServices.js.
-	// It can be omitted if it is true.
-	yesBoomsServicesFile: true,
 };
 
 module.exports = me;
