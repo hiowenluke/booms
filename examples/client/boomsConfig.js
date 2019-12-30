f58dd525cbda8d0e59d160b6c7885095b8e57e17
Owen Luke <hi.owen.luke@gmail.com>
2019-12-30 15:09:02 +0800

    Update boomsConfig.js

--- examples/client/boomsConfig.js
+++ examples/client/boomsConfig.js
@@ -13,6 +13,13 @@ const me = {
 	// If it is omitted or 'all', Booms will fetches all.
 	// Or "s1" if you just need this one.
 	servers: ['s1', 's2'],
+
+	// If it is true, the api list in file "booms/services.js" will be compact like below right.
+	// You still should to use "await" keyword to call these functions.
+	// 		s1: {										s1: {
+	//			hi: async function(name, age) {}	=> 		hi(name, age) {}
+	// 		}											}
+	isCompactApiList: false,
 };
 
 module.exports = me;
